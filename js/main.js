b17bc2bdee1c0a4b5d8b74025acbf4f924f56745
babyazalea <usun12@hotmail.com>
2019-05-14 23:03:07 +0900

    complete main page

--- js/main.js
+++ js/main.js
@@ -2,46 +2,58 @@ const noticeBtn = document.querySelector('#notice'),
     creditsBtn = document.querySelector('#credits'),
     subMenu = document.querySelector('.submenu'),
     notice = document.querySelector('.notice'),
-    credits = document.querySelector('.credits');
+    credits = document.querySelector('.credits'),
+    noticeCheck = noticeBtn.querySelector('i'),
+    creditsCheck = creditsBtn.querySelector('i');
+
 
 const ON_CN = 'on';
+const CHECKED_CN = 'checked';
 
 function creditsShow(){
-    const status= subMenu.classList.contains(ON_CN);
-    const noticeStatus= credits.classList.contains(ON_CN);
-    if(!status && !noticeStatus){
+    const suBstatus= subMenu.classList.contains(ON_CN);
+    const creditsStatus= credits.classList.contains(ON_CN);
+    const noticeStatus= notice.classList.contains(ON_CN);
+    if(!suBstatus && !creditsStatus){
         credits.classList.add(ON_CN);
         subMenu.classList.add(ON_CN);
-    } else if(status && noticeStatus){
+    } else if(suBstatus && creditsStatus){
         credits.classList.remove(ON_CN);
         subMenu.classList.remove(ON_CN);
-    } else if(status && !noticeStatus){
+    } else if(suBstatus && noticeStatus){
         credits.classList.add(ON_CN);
-    }
+        notice.classList.remove(ON_CN);
+    };
 
-    
+    creditsCheck.classList.toggle(CHECKED_CN);
+    noticeCheck.classList.remove(CHECKED_CN);
 }
 
 function creaditsClick(){
-    creditsBtn.addEventListener('click',creditsShow)
+    creditsBtn.addEventListener('click',creditsShow);
 }
 
 function noticeShow(){
-    const status= subMenu.classList.contains(ON_CN);
+    const suBstatus= subMenu.classList.contains(ON_CN);
+    const creditsStatus= credits.classList.contains(ON_CN);
     const noticeStatus= notice.classList.contains(ON_CN);
-    if(!status && !noticeStatus){
+    if(!suBstatus && !noticeStatus){
         notice.classList.add(ON_CN);
         subMenu.classList.add(ON_CN);
-    } else if(status && noticeStatus){
+    } else if(suBstatus && noticeStatus){
         notice.classList.remove(ON_CN);
         subMenu.classList.remove(ON_CN);
-    } else if(status && !noticeStatus){
+    } else if(suBstatus && creditsStatus){
         notice.classList.add(ON_CN);
-    }
+        credits.classList.remove(ON_CN);
+    };
+
+    noticeCheck.classList.toggle(CHECKED_CN);
+    creditsCheck.classList.remove(CHECKED_CN);
 }
 
 function noticeClick(){
-    noticeBtn.addEventListener('click',noticeShow)
+    noticeBtn.addEventListener('click',noticeShow);
 }
 
 noticeClick();
